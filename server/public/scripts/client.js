cb5c597dbd3499dc1d3ffba9b1e2ded74d6371b3
Luis Ortiz <l.ortizintec@gmail.com>
2023-04-15 14:06:17 -0400

    renderList function can now retrieve data from database and display it on the DOM

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -11,7 +11,19 @@ function renderList() {
     $.ajax({
         method: 'GET',
         url: '/todo'
-    }).then(function (response) {
-        console.log(response);
+    }).then(function (todoList) {
+        $('#list-container').empty();
+        for (let item of todoList) {
+            if (item.complete === 'Not Complete') {
+                $('#list-container').append(`
+                <li>${item.task}: <button>Complete?</button></li>
+                `);
+            } else {
+                $('#list-container').append(`
+                <li>${item.task}: ${item.complete}</li>
+                `);
+            }
+
+        }
     })
 }
